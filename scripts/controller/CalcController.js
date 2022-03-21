2c717cf9e51c952a6be75339aa655170d85714ea
edgarbispo <edgarbispo@hotmail.com>
2022-03-21 07:35:10 -0300

    21. C16 - Correções de Bugs com o Ponto

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -192,7 +192,7 @@ class CalcController{
             } else {
 
                 let newValue = this.getLastOperation().toString() + value.toString();
-                this.setLastOperation(parseFloat(newValue));
+                this.setLastOperation(newValue);
 
                 this.setLastNumberToDisplay();
 
@@ -209,6 +209,8 @@ class CalcController{
     addDot(){
 
         let lastOperation = this.getLastOperation();
+
+        if (typeof lastOperation === 'string' && lastOperation.split('').indexOf('.') > -1) return ;
 
         if (this.isOperator(lastOperation) || !lastOperation) {
             this.pushOperation('0.');
