82df575bfc8d227622f15580930c30c7a0c79dd7
edgarbispo <edgarbispo@hotmail.com>
2022-03-21 07:59:54 -0300

    22. C17 - Eventos de Teclado

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -16,6 +16,7 @@ class CalcController{
 
         this.initialize();
         this.initButtonsEvents();
+        this.initKeyboard();
 
     }
 
@@ -35,6 +36,53 @@ class CalcController{
 
         }, 10000);
         */
+    }
+
+    initKeyboard(){
+        document.addEventListener("keyup", e => {
+
+            switch (e.key){
+                case 'Escape':
+                    this.clearAll();
+                    break;
+
+                case 'Backespace':
+                    this.clearEntry();
+                    break;
+
+                case '+':
+                case '-':
+                case '/':
+                case '*':
+                case '%':
+                    this.addOperation(e.key);
+                    break;
+
+                case '=':
+                case 'Enter':
+                    this.calc();
+                    break;
+
+                case ".":
+                case ",":
+                    this.addDot(".");
+                    break;
+
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    this.addOperation(parseInt(e.key));
+                    break;
+
+            }
+        });
     }
 
     addEventListenerAll(element, events, fn){
