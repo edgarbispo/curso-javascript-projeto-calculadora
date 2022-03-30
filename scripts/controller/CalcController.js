b1710af506c74dcf57212411cb1143acf3d6981d
edgarbispo <edgarbispo@hotmail.com>
2022-03-30 08:13:25 -0300

    24. C19 - Trabalhando com Áudio

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -4,6 +4,8 @@ class CalcController{
 
         //A ideia do _ é para informar que é privado
 
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
         this._lastOperator = '';
         this._lastNumber = '';
 
@@ -57,6 +59,15 @@ class CalcController{
         this.setLastNumberToDisplay();
         this.pasteFromClipboard();
 
+        document.querySelectorAll('.btn-ac').forEach(btn => {
+
+            btn.addEventListener('dblclick', e =>{
+
+                this.toggleAudio();
+
+            })
+        })
+
         /*
         setTimeout(() =>{
 
@@ -66,8 +77,24 @@ class CalcController{
         */
     }
 
+    toggleAudio() {
+
+        this._audioOnOff = !this._audioOnOff;
+
+    }
+
+    playAudio(){
+
+        if (this._audioOnOff) {
+            this._audio.currentTime = 0;
+            this._audio.play();
+        }
+    }
+
     initKeyboard(){
         document.addEventListener("keyup", e => {
+
+            this.playAudio();
 
             switch (e.key){
                 case 'Escape':
@@ -302,6 +329,8 @@ class CalcController{
     }
 
     execBtn(value){
+
+        this.playAudio();
 
         switch (value){
             case 'ac':
